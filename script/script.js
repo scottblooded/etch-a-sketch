dfbe3b1c35492b0b3377e917f0d3e9a86da1a04b
Scott Livingstone <scottdlivingstone@gmail.com>
2016-12-31 13:58:50 -0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,29 @@
+function makeGrid (){
+	for (var i=0; i < gridSize * gridSize; i++) {
+		$('#canvas').append('<div class="box outline"></div>')
+	};
+
+	$('.box').css({
+		'width': $('#canvas').width() / gridSize,
+		'height': $('#canvas').height() / gridSize
+	});
+
+}
+function draw() {
+	$(".box").mouseenter(function(){
+		$(this).css("background-color", "black")
+	});
+}
+
+function clear() {
+	$('.box').css('background-color', 'white')
+
+}
+
+$(document).ready(function(){
+	gridSize = 16;
+	makeGrid();
+	draw();
+
+	$('#clear').click(clear)
+})
